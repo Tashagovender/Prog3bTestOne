25435973d2839508bba2cd0ffa2c74373ee97802
Tashagovender <st10210644@vcconnect.edu.za>
2024-09-09 10:22:34 +0200

    ci/cd pipeline

---
+++ GitActionsTester/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitActionsTester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing");
+            Console.WriteLine("BACKUP FIRST!!!");
+            Console.ReadLine();
+        }
+    }
+}
